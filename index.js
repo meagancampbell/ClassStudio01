40c9fe1e5c7ede90257b7cdfa91147a64c2ac3f6
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-07-17 19:25:21 +0000

    why is it I can look at this shit for 2 hours and not see my mistakes

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 //Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
-
 class CrewCandidate {
   constructor (name, mass, scores) {
     this.name = name;
     this.mass = mass;
     this.scores = scores;
   }
-  addScore(parameters) {
+  addScore(scores) {
       //function code
+      this.scores.push(scores);
    }
+  average(){
+  let total = 0;
+        for (let i = 0; i < this.scores.length; i++) {
+        total = total + this.scores[i];
+        total = Math.floor(total / this.scores.length * 10) / 10;
+        }
+  }
+  status(){
+    let currentStatus = '';
+    if (this.addScore() >= 90) {
+        Status = 'Accepted';
+    }if (this.addScore() < 90 && this.addScore() >= 80) {
+        Status = 'Reserve';
+    }if (this.addScore() <= 79 && this.addScore() >= 70) {
+        Status = 'Probationary';
+    }if (this.addScore() <= 69) {
+        Status = 'Rejected';
+    }
+  return console.log(`${name} earned an average test score of ${average}% and has a status of ${status}.`);
 }
 
+
 let bubbaBear = new CrewCandidate('Bubba Bear','135 kg',[88, 85, 90]);
-
 let merryMaltese = new CrewCandidate('Merry Maltese', '1.5 kg', [93, 88, 97],);
-
 let gladGator = new CrewCandidate('Glad Gator', '225 kg', [75, 78, 62]);
 
-console.log(bubbaBear);
-console.log(merryMaltese);
-console.log(gladGator);
+console.log(bubbaBear.scores);
+console.log(merryMaltese.scores);
+console.log(gladGator.scores);
 
 
-// bubbaBear.addScore(83);
+bubbaBear.addScore(83);
 // console.log(bubbaBear);
 // //Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
 // console.log(bubbaBear.average());
